9ee4edfd47629d683ce0664bcc31052b5b526f05
Danko Beribak <beribakdanko@gmail.com>
2019-11-29 21:38:22 -0800

    gif component created

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 
 import SearchBar from '../containers/search_bar';
+import Gif from '../containers/gif';
+
 
 const App = () => {
   return (
     <div className="app">
       <SearchBar />
+      <Gif />
     </div>
   );
 };
---
+++ src/containers/gif.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Gif = (props) => {
+  
+  return (
+     // const src = "https://media.giphy.com/media/LMuSfE0WEUZXbMwZfX/giphy.gif";
+    <div className="gif">
+     	<img src={"https://media.giphy.com/media/LMuSfE0WEUZXbMwZfX/giphy.gif"} className="" />
+    </div>
+  );
+};
+
+export default Gif;
