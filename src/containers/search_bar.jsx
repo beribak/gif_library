ac7e274454dd9d518c1f747ae4aa7a295cd7d8a6
Danko Beribak <beribakdanko@gmail.com>
2019-12-01 03:10:09 -0800

    search font awesome added

--- src/containers/search_bar.jsx
+++ src/containers/search_bar.jsx
@@ -17,6 +17,7 @@ class SearchBar extends Component {
 		return(	
 			<div className="search_bar_container" id="search_bar_id">
 				<h4>GIF Library</h4>
+				<i class="fas fa-search search"></i>
 				<input type="text" className="search_bar" value={this.props.inputValue} onChange={this.handleChange} />
 			</div>				
 		);
