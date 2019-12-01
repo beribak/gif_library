fa50d89fb0bcb75f74d4518190783e4d802afb25
Danko Beribak <beribakdanko@gmail.com>
2019-12-01 02:54:48 -0800

    Updates

---
+++ precache-manifest.0a00ced8144a5b2bcd199c6c91e520a8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d10ec32abdcdac84378c6a66c49d0c82",
+    "url": "/gif_library/index.html"
+  },
+  {
+    "revision": "5c64cb34407178fa3ae0",
+    "url": "/gif_library/static/css/main.ecf23ddc.chunk.css"
+  },
+  {
+    "revision": "7ef6fa64dca76c162619",
+    "url": "/gif_library/static/js/2.79694f56.chunk.js"
+  },
+  {
+    "revision": "5c64cb34407178fa3ae0",
+    "url": "/gif_library/static/js/main.d89ccecb.chunk.js"
+  },
+  {
+    "revision": "2f8155041f4121d80fd1",
+    "url": "/gif_library/static/js/runtime-main.ac5e8edc.js"
+  }
+]);
